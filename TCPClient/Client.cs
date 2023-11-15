8e1072ecb17fd916ec96eed7c5e06f8c9b7bb80b
Sayyab Khan <sayyabkhan007@gmail.com>
2023-11-15 23:01:29 +0500

    Add project files.

---
+++ TCPClient/Client.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    internal class Client
+    {
+        public static void StartClient()
+        {
+            int port = 11000;
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                Socket senderSocket = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+                senderSocket.Connect(ipEndPoint);
+                Console.WriteLine("Client is connected");
+
+                while(true)
+                {
+                    string msgFromClinet = null;
+                    Console.WriteLine("Enter a message for the server: ");
+                    msgFromClinet =  Console.ReadLine();
+
+                    
+                    
+                    senderSocket.Send(Encoding.ASCII.GetBytes(msgFromClinet), 0 , msgFromClinet.Length, SocketFlags.None);
+
+
+                    
+                    byte[] msgFromServer = new byte[1024];
+                    int size = senderSocket.Receive(msgFromServer);
+                    string data = Encoding.ASCII.GetString(msgFromServer, 0 , size);
+                    Console.WriteLine(data);
+
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ TCPServer/Server.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    internal class Server
+    {
+        
+
+       public static void StartServer()
+        {
+            int port = 11000;
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                Socket listnerSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listnerSocket.Bind(ipEndPoint);
+
+                listnerSocket.Listen(10);
+
+                Console.WriteLine("Waiting for connection...");
+                
+                Socket clientSocket = default(Socket);
+                int counter = 0;
+
+                
+                Server server = new Server();
+                while (true)
+                {
+                    counter++;
+                    clientSocket = listnerSocket.Accept();
+                    Console.WriteLine($"{counter} clients are connected");
+
+                    Thread userThread = new Thread(new ThreadStart(() => server.User(clientSocket)));
+                    userThread.Start();
+                }
+
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void User(Socket client)
+        {
+            while(true)
+            {
+                byte[] msg = new byte[1024];
+                int size = client.Receive(msg);
+                string data = Encoding.ASCII.GetString(msg, 0, size);
+                Console.WriteLine($"Data received from client =>  {data}");
+                client.Send(msg, 0, size, SocketFlags.None);
+            }
+            
+        }
+    }
+}
